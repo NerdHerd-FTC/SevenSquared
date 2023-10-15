8e6f5620a0dfe8b2295d4a8219dace690e3e35f5
ShasshU <imshassh@gmail.com>
2023-10-15 16:19:26 -0700

    Update AutoPark.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPark.java
@@ -4,12 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 @Autonomous(name = "FreightFenzy_REDAuton1 (Java)")
 public class AutoPark extends LinearOpMode {
 
-    private DcMotor RightDrive;
-    private DcMotor LeftDrive;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
 
     static final double HD_COUNTS_PER_REV = 28;
     static final double DRIVE_GEAR_REDUCTION = 20;
@@ -19,42 +25,53 @@ public class AutoPark extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    private void drive(double power, double leftInches, double rightInches) {
-        int rightTarget;
-        int leftTarget;
-
-        if (opModeIsActive()) {
-            rightTarget = RightDrive.getCurrentPosition() + (int) (rightInches * DRIVE_COUNTS_PER_IN);
-            leftTarget = LeftDrive.getCurrentPosition() + (int) (leftInches * DRIVE_COUNTS_PER_IN);
-
-            LeftDrive.setTargetPosition(leftTarget);
-            RightDrive.setTargetPosition(rightTarget);
-
-            LeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            RightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            LeftDrive.setPower(power);
-            RightDrive.setPower(power);
-
-            while (opModeIsActive() && (LeftDrive.isBusy() || RightDrive.isBusy())) {
-                // Wait until both motors are no longer busy running to position
-            }
-
-            LeftDrive.setPower(0);
-            RightDrive.setPower(0);
-        }
-    }
-
     @Override
     public void runOpMode() {
-        RightDrive = hardwareMap.get(DcMotor.class, "frontRight");
-        LeftDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
         waitForStart();
 
         if (opModeIsActive()) {
             // Move left for 12 inches
-            drive(1, -12, -12);
+            mecanumDrive(1, -12, -12, -12, -12);
+        }
+    }
+
+    private void mecanumDrive(double power, double frontLeftInches, double frontRightInches, double backLeftInches, double backRightInches) {
+        int frontLeftTarget, frontRightTarget, backLeftTarget, backRightTarget;
+
+        if (opModeIsActive()) {
+            frontLeftTarget = frontLeft.getCurrentPosition() + (int) (frontLeftInches * DRIVE_COUNTS_PER_IN);
+            frontRightTarget = frontRight.getCurrentPosition() + (int) (frontRightInches * DRIVE_COUNTS_PER_IN);
+            backLeftTarget = backLeft.getCurrentPosition() + (int) (backLeftInches * DRIVE_COUNTS_PER_IN);
+            backRightTarget = backRight.getCurrentPosition() + (int) (backRightInches * DRIVE_COUNTS_PER_IN);
+
+            frontLeft.setTargetPosition(frontLeftTarget);
+            frontRight.setTargetPosition(frontRightTarget);
+            backLeft.setTargetPosition(backLeftTarget);
+            backRight.setTargetPosition(backRightTarget);
+
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontLeft.setPower(power);
+            frontRight.setPower(power);
+            backLeft.setPower(power);
+            backRight.setPower(power);
+
+            while (opModeIsActive() && (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy())) {
+                // Wait until all motors are no longer busy running to position
+            }
+
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
         }
     }
 }
