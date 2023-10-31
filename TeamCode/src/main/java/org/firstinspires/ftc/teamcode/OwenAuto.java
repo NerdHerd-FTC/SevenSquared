d4f547c56c2ffc7d868f2420583559bf24b5a44e
Derrick K <derrick.kwan.360@gmail.com>
2023-10-30 17:04:58 -0700

    k

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OwenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OwenAuto.java
@@ -19,7 +19,7 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.eamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
