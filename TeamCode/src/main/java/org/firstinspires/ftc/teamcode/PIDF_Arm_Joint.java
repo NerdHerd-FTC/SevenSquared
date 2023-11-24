06acfd08b80756c4445dbbef27405ee83274aac7
osun24 <114966159+osun24@users.noreply.github.com>
2023-11-24 15:11:34 -0800

    Update PIDF_Arm_Joint.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_Arm_Joint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_Arm_Joint.java
@@ -27,13 +27,13 @@ public class PIDF_Arm_Joint extends LinearOpMode {
     private PIDController jointPID;
     private PIDController armPID;
 
-    public static double jointP = 0.0, jointI = 0.0, jointD = 0.0, jointF = 0.0;
-    public static double armP = 0.0, armI = 0.0, armD = 0.0, armF = 0.0;
+    public static double jointP = 0.00009, jointI = 0.0, jointD = 0.0, jointF = -0.000047;
+    public static double armP = 0.0035, armI = 0.0, armD = 0.0003, armF = 0.002;
 
     public static int jointTarget = 0, armTarget = 0;
 
-    private final double joint_ticks_per_degree = ((((1+(46.0/17))) * (1+(46.0/17))) * (1+(46.0/17)) * 28) / 360.0;
-    private final double arm_ticks_per_degree = ((((1+(46.0/17))) * (1+(46.0/11))) * 28)/360.0;
+    private final double joint_ticks_per_degree = ((((1+(46.0/17))) * (1+(46.0/17))) * (1+(46.0/17)) * 28 * 10.0/3) / 360.0;
+    private final double arm_ticks_per_degree = ((((1+(46.0/17))) * (1+(46.0/11))) * 28 * 5)/360.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -67,17 +67,42 @@ public class PIDF_Arm_Joint extends LinearOpMode {
             double joint_out = jointPID.calculate(jointMotor.getCurrentPosition(), jointTarget);
             double arm_out = armPID.calculate(armMotor.getCurrentPosition(), armTarget);
 
-            double joint_angle = jointMotor.getCurrentPosition() / joint_ticks_per_degree;
-            double relative_arm_angle = armMotor.getCurrentPosition() / arm_ticks_per_degree;
-            double arm_angle = 270 - relative_arm_angle - joint_angle;
+            double joint_angle = jointMotor.getCurrentPosition() / joint_ticks_per_degree + 193;
+            double relative_arm_angle = armMotor.getCurrentPosition() / arm_ticks_per_degree + 29;
+            double arm_angle =270 - relative_arm_angle - joint_angle;
+
+            if (arm_angle > 360) {
+                arm_angle -= 360;
+            } else if (joint_angle < 0) {
+                joint_angle += 360;
+            }
+
+            if (joint_angle > 360) {
+                joint_angle -= 360;
+            } else if (joint_angle < 0) {
+                joint_angle += 360;
+            }
 
             // joint self-torque: 16.2917713642 lb. in.
             // arm self-torque: 12.9580463969 lb. in.
-            double joint_ff = (Math.cos(Math.toRadians(joint_angle)) * 0.3793 + Math.cos(Math.toRadians(arm_angle)) * 0.6207) * jointF;
+            double arm_part_of_joint = Math.cos(Math.toRadians(arm_angle - 29)) * 6.207;
+            if (arm_part_of_joint < 0) {
+                arm_part_of_joint = 0;
+            }
+
+            if (joint_angle > 180) {
+                joint_angle = 180;
+            }
+
+            double joint_ff = (-Math.cos(Math.toRadians(joint_angle)) * 3.793 + arm_part_of_joint) * jointF;
             double arm_ff = Math.cos(Math.toRadians(arm_angle)) * armF;
 
             double joint_power = joint_out + joint_ff;
             double arm_power = arm_out + arm_ff;
+
+            if (Math.abs(gamepad1.left_stick_y) > 0.1) {
+                joint_power = gamepad1.left_stick_y * 0.7;
+            }
 
             jointMotor.setPower(Math.tanh(joint_power));
             armMotor.setPower(Math.tanh(arm_power));
@@ -89,6 +114,8 @@ public class PIDF_Arm_Joint extends LinearOpMode {
 
             telemetry.addData("Joint Angle", joint_angle);
             telemetry.addData("Arm Angle", arm_angle);
+            telemetry.addData("Relative Arm Angle", relative_arm_angle);
+            telemetry.addData("Joint Power", jointMotor.getPower());
 
             telemetry.update();
         }
