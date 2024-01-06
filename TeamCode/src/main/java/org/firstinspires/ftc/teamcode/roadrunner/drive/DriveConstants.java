a7797e92d09f46461be0a328fffd03afcc8de8ea
osun24 <114966159+osun24@users.noreply.github.com>
2024-01-05 23:59:58 -0800

    updated drive feedforward for battery

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -65,9 +65,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.002;
-    public static double kA = 0.07743;
-    public static double kStatic = 0.0123;
+    public static double kV = 0.0131;
+    public static double kA = 0.0021;
+    public static double kStatic = 0.05248;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
