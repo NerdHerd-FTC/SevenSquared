929885fe9e2706406cf0bc2ba2534878a98a2330
osun24 <114966159+osun24@users.noreply.github.com>
2024-01-09 14:14:34 -0800

    fix gitignore

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-01-09T12:44:54.528-0800";
+  public static final String APP_BUILD_TIME = "2024-01-09T14:00:23.768-0800";
 }
