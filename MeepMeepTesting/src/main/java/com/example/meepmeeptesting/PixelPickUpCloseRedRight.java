483df8767fe7a1bf2d241d510c310acdd25de2d6
Derrick K <derrick.kwan.360@gmail.com>
2024-03-02 22:57:54 -0800

    Created New PixelPickUpFarPixelPickUp Trajectories

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PixelPickUpCloseRedRight.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PixelPickUpCloseRedRight.java
@@ -5,7 +5,8 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-public class PixelPickUpCloseBlueCenter {
+
+public class PixelPickUpCloseRedRight {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -13,19 +14,19 @@ public class PixelPickUpCloseBlueCenter {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(52.48291908330528, 52.48291908330528, 3.114857287413855, Math.toRadians(190.94804165608335), 19)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12, 63, Math.toRadians(270)))
-                                .splineTo(new Vector2d(12, 30), Math.toRadians(270))
-                                .splineToConstantHeading(new Vector2d(12, 50), Math.toRadians(270))
-                                .splineTo(new Vector2d(50.5, 26.5), Math.toRadians(0))
-                                .strafeRight(28)
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -63, Math.toRadians(90)))
+                                .splineTo(new Vector2d(20, -30), Math.toRadians(90))
+                                .splineToConstantHeading(new Vector2d(23, -48), Math.toRadians(90))
+                                .splineToSplineHeading(new Pose2d(51, -36, Math.toRadians(0)), Math.toRadians(0))
+                                .strafeLeft(24)
                                 .turn(Math.toRadians(180))
-                                .splineTo(new Vector2d(-10, 3), Math.toRadians(180))
-                                .splineTo(new Vector2d(-47.5, 28), Math.toRadians(180))
+                                .splineTo(new Vector2d(-10, -3), Math.toRadians(180))
+                                .splineTo(new Vector2d(-47.5, -28), Math.toRadians(180))
                                 .turn(Math.toRadians(180))
-                                .splineTo(new Vector2d(-16, 10), Math.toRadians(0))
-                                .splineTo(new Vector2d(20, 13), Math.toRadians(0))
-                                .splineTo(new Vector2d(50, 26.5), Math.toRadians(0))
-
+                                .splineTo(new Vector2d(-16, -10), Math.toRadians(0))
+                                .splineTo(new Vector2d(20, -13), Math.toRadians(0))
+                                .splineTo(new Vector2d(54, -24), Math.toRadians(0))
+                                .strafeRight(38)
                                 .build()
                 );
 
@@ -35,6 +36,4 @@ public class PixelPickUpCloseBlueCenter {
                 .addEntity(myBot)
                 .start();
     }
-
 }
-
